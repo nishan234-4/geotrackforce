8741caed4fec1e5a632ae718f3e64623daf07223
DELL <DELL@DESKTOP-TP1QMSA>
2025-07-04 12:43:34 +0545

    Fixed launcher icon issue and update launcher icon

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:geo_trackforce/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Myapp() as Widget);
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -27,4 +27,11 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+}
+
+WidgetConstMyapp() {
+}
+
+class Myapp {
+  const Myapp();
 }
